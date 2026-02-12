ffcbe65f3d6b37990fb649e0a958de2e75f58e0b
ricardoberetella <ricardoberetella20@gmail.com>
2026-02-11 21:36:27 -0300

    Delete services directory

--- services/database.ts
+++
@@ -1,4 +0,0 @@
-
-// Este arquivo agora serve apenas como fallback ou pode ser removido.
-// O projeto está utilizando serviços do services/firebase.ts para armazenamento Cloud.
-export const DatabaseService = {};
