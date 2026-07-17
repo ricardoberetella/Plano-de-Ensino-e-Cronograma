f2f34aa071b25b4627db34b8639a764af5248680
ricardoberetella <ricardoberetella20@gmail.com>
2026-07-17 14:22:11 -0300

    Update and rename index.tsx to main.tsx

---
+++ main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = ReactDOMClient.createRoot(container);
+  root.render(
+    React.createElement(React.StrictMode, null, 
+      React.createElement(App, null)
+    )
+  );
+}
